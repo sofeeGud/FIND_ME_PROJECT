1a70e7f917d8ff5373b164abe3a5f984e5f673d7
Lenovo T540p <sonychkalapochka94@gmail.com>
2020-08-28 21:27:04 +0300

    hw2 task2 +errors

--- src/main/java/com/findme/controller/UserController.java
+++ src/main/java/com/findme/controller/UserController.java
@@ -2,6 +2,7 @@ package com.findme.controller;
 
 import com.findme.models.User;
 import com.findme.service.UserService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpServerErrorException;
 
 @Controller
 public class UserController {
@@ -21,11 +23,18 @@ public class UserController {
     }
 
     @RequestMapping(path="/user/{id}", method = RequestMethod.GET)
-    public String profile (Model model,@PathVariable String id) throws Exception {
-
+    public String profile (Model model,@PathVariable String id) throws HttpServerErrorException.InternalServerError, NotFoundException {
+        try {
             User user = userService.findById(Long.parseLong(id));
-
+            if (user != null) {
                 model.addAttribute("user", user);
                 return "profile";
+            }
+            return "404";
+
+        } catch (HttpServerErrorException.InternalServerError e) {
+
+            return "500";
+        }
     }
 }
