3a3d0c6237fccde00da2099614943cfa9963d77c
Lenovo T540p <sonychkalapochka94@gmail.com>
2020-09-23 22:34:13 +0300

    hw2 task2 +errors

--- src/main/java/com/findme/dao/PostDAO.java
+++ src/main/java/com/findme/dao/PostDAO.java
@@ -3,13 +3,8 @@ package com.findme.dao;
 import com.findme.models.Post;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Repository
 public class PostDAO extends GeneralDAOImpl<Post> {
 
-
-    @PersistenceContext
-    private EntityManager entityManager;
 }
