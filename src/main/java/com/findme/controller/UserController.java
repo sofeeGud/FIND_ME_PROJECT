6b3cc83f7bdd75c6d7795ecf657cffd32bf1b055
Lenovo T540p <sonychkalapochka94@gmail.com>
2020-10-24 13:23:58 +0300

    hw4

--- src/main/java/com/findme/controller/UserController.java
+++ src/main/java/com/findme/controller/UserController.java
@@ -51,7 +51,7 @@ public class UserController {
         }
     }
 
-    @RequestMapping(path = "/users", method = RequestMethod.PUT)
+    @RequestMapping(path = "/users", method = RequestMethod.POST)
     public ResponseEntity<String> register(@ModelAttribute User user) {
         try {
             userService.save(user);
@@ -66,7 +66,7 @@ public class UserController {
     }
 
 
-    @RequestMapping(path = "/users", method = RequestMethod.POST)
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
     public ResponseEntity<String> login(HttpSession session, HttpServletRequest request) {
         try {
             User user = userService.authorization(request.getParameter("email"), request.getParameter("password"));
